692be92414e9b29eefe7b28ca5c16fdcd3c2eecf
Hyungjin Kim <hjux07@gmail.com>
2022-11-11 11:12:25 +0900

    ambientLight로 전체 밝기 조절 (그림자 생성X) / 박스, 바닥, 카메라 위치값 변경

--- src/App.js
+++ src/App.js
@@ -2,33 +2,33 @@ import "./scss/style.scss";
 import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-extend({ OrbitControls }); //  orbitControls를 확장시켜서 사용가능하게 함
+extend({ OrbitControls });
 
 //화면축을 변경할 수 있는 컴포넌트
 const Orbit = () => {
-  const { camera, gl } = useThree(); // react에서 Three.js 기능을 사용하기 위한 useTree();
+  const { camera, gl } = useThree();
   return <orbitControls args={[camera, gl.domElement]} />;
 };
 
 const Box = (props) => {
-  const ref = useRef(null); // Box참조
-
-  // 1초마다 프레임 렌더링하며 useFrame을 활용해 모션을 구현한다.
-  // 콜백함수에 인자로 state객체가 들어온다.
+  const ref = useRef(null);
   useFrame(() => {
     ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
-
   return (
-    // 전달된 props를 rest parameter를 이용해서 한꺼번에 "모두 적용"
     <mesh ref={ref} {...props}>
-      
-      {/* 정육면체 도형 가져오기 */}
       <boxBufferGeometry />
+      <meshPhysicalMaterial color="blue" />
+    </mesh>
+  );
+};
 
-      {/* 기본 색상 지정 */}
-      <meshBasicMaterial color="blue" />
+const Floor = (props) => {
+  return (
+    <mesh {...props}>
+      <boxBufferGeometry args={[20, 1, 10]} />
+      <meshPhysicalMaterial color="white" />
     </mesh>
   );
 };
@@ -36,20 +36,13 @@ const Box = (props) => {
 function App() {
   return (
     <figure>
-      <Canvas
-        style={{ background: "#111" }}
-        // 카메라 시점 x, y, z
-        camera={{ position: [7, 7, 7] }}
-      >
-        {/* 축 기준점 가이드 */}
+      <Canvas style={{ background: "#111" }} camera={{ position: [3, 3, 3] }}>
         <axesHelper args={[6]} />
-
-        {/* 마우스 드래그로 시점을 컨트롤 할 수 있게함 */}
         <Orbit />
 
-        {/* Box컴포넌트 호출시 위치값을 Props로 전달 */}
-        {/* mesh의 position 속성에 배열로 값(x, y, z)로 위치 이동시킬 수 있다. */}
-        <Box position={[0, 2, 0]} />
+        <ambientLight intensity={0.2} />
+        <Box position={[-1, 1, 2]} />
+        <Floor position={[0, -0.5, 0]} />
       </Canvas>
     </figure>
   );
