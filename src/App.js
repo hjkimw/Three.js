a9c702232054fc333cfbe98c894fcd4ab719ffa7
Hyungjin Kim <hjux07@gmail.com>
2022-11-11 11:23:53 +0900

    포인트 라이트 설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./scss/style.scss";
 import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useRef } from "react";
+import { MeshPhongMaterial } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 extend({ OrbitControls });
 
-//화면축을 변경할 수 있는 컴포넌트
 const Orbit = () => {
   const { camera, gl } = useThree();
   return <orbitControls args={[camera, gl.domElement]} />;
 };
-
+const Bulb = (props) => {
+  return (
+    <mesh {...props}>
+      <pointLight castShadow intensity={1} />
+      {/* 반지름, 가로면분할, 세로면분할 */}
+      <sphereBufferGeometry args={[0.5, 20, 20]} />
+      <meshPhongMaterial emissive="yellow" />
+    </mesh>
+  );
+};
 const Box = (props) => {
   const ref = useRef(null);
   useFrame(() => {
@@ -17,30 +26,34 @@ const Box = (props) => {
     ref.current.rotation.y += 0.01;
   });
   return (
-    <mesh ref={ref} {...props}>
+    <mesh ref={ref} {...props} receiveShadow castShadow>
       <boxBufferGeometry />
       <meshPhysicalMaterial color="blue" />
     </mesh>
   );
 };
-
 const Floor = (props) => {
   return (
-    <mesh {...props}>
+    <mesh {...props} receiveShadow>
       <boxBufferGeometry args={[20, 1, 10]} />
       <meshPhysicalMaterial color="white" />
     </mesh>
   );
 };
-
 function App() {
   return (
     <figure>
-      <Canvas style={{ background: "#111" }} camera={{ position: [3, 3, 3] }}>
+      <Canvas
+        //그림자 설정시 필요함
+        shadowMap
+        style={{ background: "#111" }}
+        camera={{ position: [4, 4, 4] }}
+      >
         <axesHelper args={[6]} />
         <Orbit />
 
         <ambientLight intensity={0.2} />
+        <Bulb position={[0, 3, 0]} />
         <Box position={[-1, 1, 2]} />
         <Floor position={[0, -0.5, 0]} />
       </Canvas>
