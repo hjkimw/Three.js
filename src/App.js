2309e3052095d5cf9f1053a6a5d43f4f7364d947
Hyungjin Kim <hjux07@gmail.com>
2022-11-11 10:13:58 +0900

    캔버스 설정 및 기본축 설정, 카메라 위치지정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './scss/style.scss';
+import "./scss/style.scss";
+import Orbit from "./components/Orbit";
+import { Canvas, extend } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+extend({ OrbitControls });
 
 function App() {
-
-	return (
-		<figure>
-
-		</figure>
-	);
+  return (
+    <figure>
+      <Canvas
+        shadowMap
+        style={{ background: "#111" }}
+        // 카메라 시점 x, y, z
+        camera={{ position: [7, 7, 7] }}
+      >
+        {/* 궤도 Orbit */}
+        <Orbit />
+        {/* 시점을 보여주는 axesHelper */}
+        <axesHelper args={[6]} />
+      </Canvas>
+    </figure>
+  );
 }
 
 export default App;
